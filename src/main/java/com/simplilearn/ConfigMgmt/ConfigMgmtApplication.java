3dff0a20979f13a7d43cf21ed75f00bf36ab716f
amikshas <amiksha.thakkar@gmail.com>
2021-09-15 18:07:20 +0000

    Initial Commit

---
+++ src/main/java/com/simplilearn/ConfigMgmt/ConfigMgmtApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn.ConfigMgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigMgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigMgmtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplilearn/ConfigMgmt/ConfigMgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.ConfigMgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigMgmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
